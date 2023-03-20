e6a06783ce873a19dcd0a96e5486dbaa447f26a9
Kseniya Manchyk <kseniya.manchyk@thomsonreuters.com>
2023-03-20 16:58:16 +0300

    Complete task 1

---
+++ CartingService/BLL/Validation/CartValidator.cs
@@ -0,0 +1,13 @@
+﻿using CartingService.Domain;
+using FluentValidation;
+
+namespace CartingService.BLL.Validation
+{
+    public class CartValidator : AbstractValidator<Cart>
+    {
+        public CartValidator()
+        {
+            RuleFor(cart => cart.Id).NotEmpty().WithMessage("Cart Id is required.");
+        }
+    }
+}
